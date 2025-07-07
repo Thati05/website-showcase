4e26c77b11445e8a7d85a19ec45fd24b8dead9c6
Thati05 <m.seithati01@gmail.com>
2025-07-07 21:29:24 +0200

    commit

--- components/WebsiteVideoShowcase.jsx
+++ components/WebsiteVideoShowcase.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function WebsiteVideoShowcase() {
   return (
-    <div>WebsiteVideoShowcase</div>
+  <div className='min-h-screen flex items-center bg-gradient-to-tr from-red-900 via-black to-red-800 justify-center p-8'>
+    <div className=' rounded-xl  overflow-hidden  max-w-4xl w-full '>
+      <video autoPlay className='w-full h-auto' src='\assets\videos\Quality.mp4'>
+
+      </video>
+
+    </div>
+
+  </div>
   )
 }
